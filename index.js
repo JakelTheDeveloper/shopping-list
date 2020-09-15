5c0e9a0013e8ad5c39ff0c78ee7147bd33333c97
Ja'Kel Jenkins <68311826+JakelTheDeveloper@users.noreply.github.com>
2020-09-15 18:30:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function main(){
+    $('#js-shopping-list-form').submit(function(evt){
+    evt.preventDefault();
+   
+    let input = $('input#shopping-list-entry').val();
+   let  template = `<li>
+    <span class="shopping-item">${input}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
+  $(`.shopping-list`).append(template);
+    });
+}
+$(`.shopping-list`).on("click",".shopping-item-toggle", function(){
+    let Title = $(this).parent().siblings();
+    console.log(Title);
+    Title.toggleClass('shopping-item__checked');
+});
+
+$(`.shopping-list`).on("click",".shopping-item-delete", function(){
+    let current = $(this).closest('li');
+    console.log(current);
+    current.remove();
+});
+$(main);
